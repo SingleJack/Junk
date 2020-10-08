41593ac113274170d7f23e7769a285ae0cdb34af
Roy Lilly <roylilly123@gmail.com>
2020-10-08 09:56:50 -0700

    User login authentication

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using SchedulerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchedulerApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public string Login(UserModel userModel)
+        {
+            return "Results: Username =" + userModel.Username + "PW = " + userModel.Password;
+        }
+    }
+}
---
+++ Controllers/MainController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchedulerApp.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            //return a 'mainview.cshtml' - view maps to the action method name
+            return View();
+        }
+
+        public ActionResult ErrorMessage()
+        {
+            //return a 'mainview.cshtml' - view maps to the action method name
+            return View();
+        }
+    }
+}
